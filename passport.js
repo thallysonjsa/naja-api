f0ea7ff9a550bac5f78622f8be726d47b25089da
thallysonjsa <thallyson.alves@ccc.ufcg.edu.br>
2019-10-02 13:15:33 -0300

    modificando require

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GooglePlusTokenStrategy = require('passport-google-plus-token');
-const User = require('../naja-api/models/User');
+const User = require('./models/User');
 
 passport.use('googleToken', new GooglePlusTokenStrategy({
     clientID: '296627314306-n5v80i3gjk5tnjtqbdv3dh46po08qekp.apps.googleusercontent.com',
@@ -10,7 +10,7 @@ passport.use('googleToken', new GooglePlusTokenStrategy({
         const existingUser = await User.findOne({ "google.id": profile.id });
 
         if (existingUser) {
-            console.log('Ja existe, irmão');
+            console.log('Usuário já cadastrado!');
             return done(null, existingUser);
         }
 
